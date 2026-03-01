f7855237e1e4b0c5dcc84b4e39dfd2e0ead3dabb
ViVi09030 <vivianarango07@gmail.com>
2026-02-28 22:32:18 -0500

    feat: add image upload routes using injected storage

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .utils import ImageLocalStorage
 from .views import (
     HomePageView,
     AboutPageView,
@@ -8,6 +9,7 @@ from .views import (
     ProductCreateView,
     CartView,
     CartRemoveAllView,
+    ImageViewFactory,
 )
 
 urlpatterns = [
@@ -20,4 +22,6 @@ urlpatterns = [
     path("cart/", CartView.as_view(), name="cart_index"),
     path("cart/add/<str:product_id>", CartView.as_view(), name="cart_add"),
     path("cart/removeAll", CartRemoveAllView.as_view(), name="cart_removeAll"),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
 ]
